7c912c3a0f4b29078406f8d0218b22691975fabe
Rofael-Alfons <rofa.alfons7@gmail.com>
2023-02-15 21:25:58 +0200

    my changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevList) => [...prevList, { name: uName, age: uAge }]);
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UsersList users={usersList}></UsersList>
+    </div>
+  );
+}
+
+export default App;
